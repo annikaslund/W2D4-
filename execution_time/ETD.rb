296b0fa5e1416cee8ac3b25242761958424fdc66
annika <annikaslund@gmail.com>
2018-10-10 11:56:08 -0700

    Complete largest_contiguous_sum phase 1

--- execution_time/ETD.rb
+++ execution_time/ETD.rb
@@ -1,4 +1,6 @@
-def my_min(arr)
+# O(n^2) - quadratic
+
+def my_min_1a(arr)
   smallest = arr.first
 
   (0...arr.length-1).each do |idx|
@@ -14,7 +16,43 @@ def my_min(arr)
   smallest
 end
 
-def my_min(arr)
+# O(log n) ?
+
+def my_min_1b(arr)
   sorted = arr.sort
   sorted.first
 end
+
+# O(n)
+
+def my_min_2(arr)
+  min = arr.first
+
+  arr.each { |num| min_num = num if num < min_num }
+
+  min
+end
+
+def largest_contiguous_subarr_1(arr)
+  subsets = all_subsets(arr)
+  max_total = subsets.first.reduce(:+)
+
+  subsets.each do |set|
+    max_total = set.reduce(:+) if set.reduce(:+) > max_total
+  end
+
+  max_total
+end
+
+def all_subsets(arr)
+  sets = []
+    (0...arr.length).each do |start_pos|
+     sub_sets = []
+      (start_pos...arr.length).each do |end_pos|
+       sub_sets << arr[start_pos..end_pos]
+      end
+      sets += sub_sets
+    end
+
+  sets
+end 
