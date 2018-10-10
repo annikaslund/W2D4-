ee68de44f4a870cf07bf6aa52ba0c02e3ed0c3af
annika <annikaslund@gmail.com>
2018-10-10 14:19:30 -0700

    Complete #second_anagram? phase 2

--- anagrams/anagrams.rb
+++ anagrams/anagrams.rb
@@ -1,6 +1,20 @@
+# O(n!)
 def first_anagram?(word1, word2)
   word_arr = word1.chars
   permutations = word_arr.permutation.to_a
   anagrams = permutations.map { |perm| perm.join('') }
   return anagrams.include?(word2)
+end
+
+#O(n)
+def second_anagram?(word1, word2)
+  return false if word1.length != word2.length
+  word2 = word2.chars
+
+  word1.chars.each do |char|
+    idx = word2.find_index(char)
+    word2.delete_at(idx)
+  end
+
+  word2.empty?
 end
